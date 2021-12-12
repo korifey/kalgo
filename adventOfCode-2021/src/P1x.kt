97007ec832e57b24aff0aaa04834183550a99048
Dmitry Ivanov <Dmitry.Ivanov@huawei.com>
2021-12-12 18:11:43 +0300

    p1x

--- adventOfCode-2021/src/P1x.kt
+++ adventOfCode-2021/src/P1x.kt
@@ -1,6 +1,7 @@
 
 fun main(args: Array<String>) {
     val a = input.readLines().map { it.toInt() }
-    val res = a.withIndex().count { (i, v) -> i>0 && v > a[i-1] }
+    val b = (0..a.lastIndex-2).map { i -> a[i] + a[i+1] + a[i+2] }
+    val res = b.indices.drop(1).count { b[it] > b[it-1] }
     println(res)
 }
