3dfea98f807e7f146bab1e357212e3f6eec6f883
Arcady Ivanov <arcadyivanov@gmail.com>
2021-12-26 05:01:06 +1200

    P19 done

--- adventOfCode-2021/src/task14x/Task14x.java
+++ adventOfCode-2021/src/task14x/Task14x.java
@@ -26,7 +26,7 @@ class Rule {
 
 public class Task14x {
     ArrayList<Rule> rules = new ArrayList<>();
-    String dataFileName = "adventOfCode-2021/resources/input14x.txt";
+    String dataFileName = "adventOfCode-2021/resources/input14test.txt";
     public Task14x() throws Exception {
 
         // + parse input data
